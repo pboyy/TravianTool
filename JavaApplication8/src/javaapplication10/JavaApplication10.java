babb72702012e3c3fe979263e7c90f50694fb0fd
SerbianTravianDude <balkanac021@gmail.com>
2017-11-06 09:23:41 +0100

    hello p

--- JavaApplication8/src/javaapplication10/JavaApplication10.java
+++ JavaApplication8/src/javaapplication10/JavaApplication10.java
@@ -3,42 +3,47 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package newpackage;
+package javaapplication10;
 
-//import statements
-
-import com.sun.rowset.internal.Row;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
-import javafx.scene.control.Cell;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class NewClass
-{
-    public static void main(String[] args)
-    {
-        //Blank workbook
-       XSSFSheet sheet = workbook.createSheet("Employee Data");
+/**
+ *
+ * @author Pieter
+ */
+public class JavaApplication10 {
 
-         
-          
-        //This data needs to be written (Object[])
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     * @throws org.apache.poi.openxml4j.exceptions.InvalidFormatException
+     */
+    public static void main(String[] args) throws IOException,InvalidFormatException  {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("goal");
+        
         Map<String, Object[]> data = new TreeMap<>();
         data.put("1", new Object[] {"ID", "NAME", "LASTNAME"});
         data.put("2", new Object[] {1, "Amit", "Shukla"});
         data.put("3", new Object[] {2, "Lokesh", "Gupta"});
         data.put("4", new Object[] {3, "John", "Adwards"});
         data.put("5", new Object[] {4, "Brian", "Schultz"});
-          
-        //Iterate over data and write to sheet
-        Set<String> keyset = data.keySet();
+        
+     Set<String> keyset = data.keySet();
         int rownum = 0;
         for (String key : keyset)
         {
-            Row row = sheet.createRow(rownum++);
+            XSSFRow row  = (XSSFRow) sheet.createRow(rownum++);
             Object [] objArr = data.get(key);
             int cellnum = 0;
             for (Object obj : objArr)
@@ -52,16 +57,12 @@ public class NewClass
                     FileOutputStream out = new FileOutputStream(new File("java.xlsx"))) {
                 workbook.write(out);
             }
-            System.out.println("java.");
+            System.out.println("java"+workbook);
         }
         catch (IOException e)
         {
         }
+                
     }
+    
 }
-/**
- *
- * @author Pieter
- */
-
-
